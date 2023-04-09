7406c751ef33c96c3e9ea228caae8665decc7429
githiago-f <tfarias@protonmail.com>
2023-04-09 15:48:38 -0300

    remove unused import

--- src/main/java/ai/recarrega/locationservice/core/domain/carregadores/service/PontoDeRecargaService.java
+++ src/main/java/ai/recarrega/locationservice/core/domain/carregadores/service/PontoDeRecargaService.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
