6a0e9f30c70d27a65a610e1b4eb65c9e2f016d7a
githiago-f <tfarias@protonmail.com>
2023-04-16 16:15:11 -0400

    fix cors

--- src/main/java/ai/recarrega/locationservice/core/application/configuration/WebConfig.java
+++ src/main/java/ai/recarrega/locationservice/core/application/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package ai.recarrega.locationservice.core.application.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import ai.recarrega.locationservice.infra.StatusTomadaConverter;
@@ -15,5 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
         log.info("Register converter for StatusTomada");
         registry.addConverter(new StatusTomadaConverter());
         WebMvcConfigurer.super.addFormatters(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
