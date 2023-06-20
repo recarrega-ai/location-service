b1f6361faf0f045e6d9390e973838a373569ab49
githiago-f <tfarias@protonmail.com>
2023-06-19 22:37:38 -0400

    feat: socket vacancy

--- src/main/java/ai/recarrega/locationservice/application/controller/TomadaController.java
+++ src/main/java/ai/recarrega/locationservice/application/controller/TomadaController.java
@@ -2,30 +2,45 @@ package ai.recarrega.locationservice.application.controller;
 
 import ai.recarrega.locationservice.application.controller.dto.TomadaDTO;
 import ai.recarrega.locationservice.domain.carregadores.vo.StatusTomada;
+import ai.recarrega.locationservice.infra.data.PontosDeRecargaRepository;
 import ai.recarrega.locationservice.infra.data.TomadasRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/tomadas")
 public class TomadaController {
     private final TomadasRepository tomadasRepository;
+	private final PontosDeRecargaRepository pontosRepository;
 
     @Autowired
-    public TomadaController(TomadasRepository tomadasRepository) {
+    public TomadaController(
+		TomadasRepository tomadasRepository,
+		PontosDeRecargaRepository pontosRepository
+	) {
         this.tomadasRepository = tomadasRepository;
+		this.pontosRepository = pontosRepository;
     }
 
-    @PatchMapping("/{tomadaId}/{status}")
-    public ResponseEntity<TomadaDTO> atualizarStatus(
-        @PathVariable("tomadaId") Long tomadaId,
-        @PathVariable("status") StatusTomada status
-    ) {
-        return tomadasRepository.findById(tomadaId).map(tomada -> {
-            tomada.setStatus(status);
-            return ResponseEntity.ok(tomadasRepository.save(tomada).toDTO());
-        }).orElseGet(() -> ResponseEntity.notFound().build());
+    @PatchMapping("/{pontoId}/em_uso")
+    public ResponseEntity<?> ocuparTomada(@PathVariable("pontoId") Long pontoId) {
+		var ponto = pontosRepository.findById(pontoId);
+		if(ponto.isEmpty()) {
+			return ResponseEntity.badRequest()
+				.body("Ponto não encontrado!");
+		}
+        return tomadasRepository.findByStatusAndPonto(StatusTomada.LIVRE, ponto.get())
+			.stream().findFirst()
+			.map(tomada -> {
+				log.info("Tomada: {}", tomada.getId());
+				tomada.setStatus(StatusTomada.EM_USO);
+				tomadasRepository.save(tomada);
+				return ResponseEntity.ok(tomada.toDTO());
+			})
+			.orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/ai/recarrega/locationservice/infra/data/TomadasRepository.java
+++ src/main/java/ai/recarrega/locationservice/infra/data/TomadasRepository.java
@@ -1,10 +1,15 @@
 package ai.recarrega.locationservice.infra.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ai.recarrega.locationservice.domain.carregadores.Ponto;
 import ai.recarrega.locationservice.domain.carregadores.Tomada;
+import ai.recarrega.locationservice.domain.carregadores.vo.StatusTomada;
 
 @Repository
 public interface TomadasRepository extends JpaRepository<Tomada, Long> {
+	List<Tomada> findByStatusAndPonto(StatusTomada status, Ponto ponto);
 }
